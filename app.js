92c599ec84d94a3a17b5f7bbdfd395b36c39d685
gigigihyun <01094438134a@gmail.com>
2026-02-13 22:09:42 +0900

    login signup
    dddd

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./database');
+const userRouter = require("./src/routes/userRoutes");
 
 // 환경변수(.env) 로딩
 dotenv.config();
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 // 미들웨어 설정
 app.use(cors()); // 모든 도메인 요청 허용 (플러터 앱 연동 필수)
 app.use(express.json()); // JSON 데이터 해석
+
+app.use('/api/users', userRouter);
+
 
 // 기본 접속 테스트
 app.get('/', (req, res) => {
