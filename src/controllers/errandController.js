5e695a62f2cca23456ae2855c9f5e2aaccf82724
sungBin <bin022130@naver.com>
2026-03-09 22:52:03 +0900

    dd
    d

--- src/controllers/errandController.js
+++ src/controllers/errandController.js
@@ -5,8 +5,16 @@ dotenv.config();
 
 exports.createErrand = async (req, res) => {
   try {
-    const { user_id, title, content, cost, deadline, tags, image_url } = req.body;
-    // status는 방금 등록했으니 'WAITING'으로 고정.
+    const user_id = req.body.user_id || 1;
+    const title = req.body.title || '제목 없음';
+    
+    const content = req.body.content || ''; 
+    const cost = req.body.cost || 0;
+    
+    const deadline = req.body.deadline || null;
+    const tags = req.body.tags || null; 
+    const image_url = req.body.image_url || null;
+
     const sql = `
       INSERT INTO posts (
         user_id, title, content, cost, status, deadline, tags, image_url
@@ -19,21 +27,20 @@ exports.createErrand = async (req, res) => {
       content, 
       cost, 
       deadline, 
-      JSON.stringify(tags), // 배열이나 객체를 JSON 문자열로 변환.
+      tags, 
       image_url
     ];
 
-    // const [result] = await db.execute(sql, values);
     const [result] = await db.promise().execute(sql, values);
 
     res.status(201).json({
       success: true,
       message: "등록 성공",
-      errandId: result.insertId // 새로 생성된 심부름의 ID.
+      errandId: result.insertId // 새로 생성된 심부름의 ID
     });
 
   } catch (error) {
-    console.error("등록 실패", error);
+    console.error("등록실패 : ", error);
     res.status(500).json({
       success: false,
       message: "서버 이상 있음"
