451cf33c10abaeb0517561127a3ae0197b84e9b4
sungBin <bin022130@naver.com>
2026-03-16 00:00:05 +0900

    modify errandController
    게시물 생성 기능 수정함.

--- src/controllers/errandController.js
+++ src/controllers/errandController.js
@@ -5,26 +5,30 @@ dotenv.config();
 
 exports.createErrand = async (req, res) => {
   try {
-    const user_id = req.body.user_id || 1;
+    if (!req.user || !req.user.id) {
+        return res.status(401).json({ success: false, message: "로그인이 된 상태여야 합니다." });
+    }
+
+    const user_id = req.user.id;    
     const title = req.body.title || '제목 없음';
-    
     const content = req.body.content || ''; 
+    const location = req.body.location || '';
     const cost = req.body.cost || 0;
-    
     const deadline = req.body.deadline || null;
     const tags = req.body.tags || null; 
     const image_url = req.body.image_url || null;
 
     const sql = `
       INSERT INTO posts (
-        user_id, title, content, cost, status, deadline, tags, image_url
-      ) VALUES (?, ?, ?, ?, 'WAITING', ?, ?, ?)
+        user_id, title, content, location, cost, status, deadline, tags, image_url
+      ) VALUES (?, ?, ?, ?, ?, 'WAITING', ?, ?, ?)
     `;
 
     const values = [
       user_id, 
       title, 
       content, 
+      location,
       cost, 
       deadline, 
       tags, 
@@ -36,7 +40,7 @@ exports.createErrand = async (req, res) => {
     res.status(201).json({
       success: true,
       message: "등록 성공",
-      errandId: result.insertId // 새로 생성된 심부름의 ID
+      errandId: result.insertId 
     });
 
   } catch (error) {
