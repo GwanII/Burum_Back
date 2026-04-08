e21f020e3ebcccf841b03cfd1cea9a1d1a053bae
gigigihyun <01094438134a@gmail.com>
2026-04-08 18:01:48 +0900

    google ai
    ai 기능 근데 싹 수정될듯

--- src/routes/errandRoutes.js
+++ src/routes/errandRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const errandController = require('../controllers/errandController');
+const recommendController = require('../controllers/recommendController');
 const multer = require('multer');
 
 const { verifyToken } = require('../middlewares/authMiddleware');
@@ -8,6 +9,7 @@ const { verifyToken } = require('../middlewares/authMiddleware');
 const upload = multer({dest: 'upload/' });
 
 router.post('/', verifyToken, upload.array('images', 10), errandController.createErrand);
+router.post('/recommend-price', verifyToken, recommendController.recommendPrice);
 router.put('/:postId/assign', verifyToken, errandController.assignErrand);
 router.put('/:postId/cancelAssign', verifyToken, errandController.cancelAssignErrand);
 
