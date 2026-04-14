593102124e580b32ec560bb4e645095bdf1bba08
ddany03 <kdy8261@gmail.com>
2026-04-15 05:32:53 +0900

    fdg
    fg

--- src/routes/errandRoutes.js
+++ src/routes/errandRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const errandController = require('../controllers/errandController');
 const multer = require('multer');
+const recommendController = require('../controllers/recommendController');
 
 const { verifyToken } = require('../middlewares/authMiddleware');
 
@@ -10,5 +11,10 @@ const upload = multer({dest: 'upload/' });
 router.post('/', verifyToken, upload.array('images', 10), errandController.createErrand);
 
 // router.get('/', errandController.getErrands);
+router.post('/recommend-price', verifyToken, recommendController.recommendPrice);
+router.put('/:postId/assign', verifyToken, errandController.assignErrand);
+router.put('/:postId/cancelAssign', verifyToken, errandController.cancelAssignErrand);
+router.put('/:postId/complete', verifyToken, errandController.completeErrand);
 
+module.exports = router;
 module.exports = router;
